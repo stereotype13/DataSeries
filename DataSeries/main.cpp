86a487b45ce2c59a3aa62468d311a9220b10343f
Robert Hodel <rhodel3@gatech.edu>
2019-05-13 13:56:05 -0700

    Rule of Five

--- DataSeries/main.cpp
+++ DataSeries/main.cpp
@@ -10,10 +10,55 @@ private:
 	std::vector<std::shared_ptr<T>> _vector;
 	std::map<std::string, std::shared_ptr<T>> _map;
 
+	void makeDeepCopy(const DataSeries<T>& other) {
+		std::vector<std::shared_ptr<T>> _newVector;
+		std::map<std::string, std::shared_ptr<T>> _newMap;
+
+		for (const auto& i : other._map) {
+			std::pair<std::string, std::shared_ptr<T>> tempPair;
+			tempPair.first = i.first;
+			tempPair.second = std::make_shared<T>(*(i.second));
+			_newMap.emplace(tempPair);
+		}
+
+		for (const auto& i : other._vector) {
+			_newVector.push_back(std::make_shared<T>(*i));
+		}
+
+		this->_vector.clear();
+		this->_map.clear();
+		this->_vector = _newVector;
+		this->_map = _newMap;
+	}
+
 public:
 
-	DataSeries<T>() {
-		//_vector.push_back(std::shared_ptr<T>());
+	DataSeries<T>() = default;
+
+	DataSeries<T>(const DataSeries<T>& other) {
+		//make a deep copy
+		makeDeepCopy(other);
+	}
+
+	DataSeries<T>& operator=(const DataSeries<T>& other) {
+		if (this == &other)
+			return *this;
+
+		//make a deep copy
+		makeDeepCopy(other);
+		return *this;
+	}
+
+	DataSeries<T>(DataSeries<T>&& other) {
+		std::swap(_vector, other._vector);
+		std::swap(_map, other._map);
+	}
+
+	DataSeries<T>& operator=(DataSeries<T>&& other) {
+		std::swap(_vector, other._vector);
+		std::swap(_map, other._map);
+
+		return *this;
 	}
 
 	T& operator[](long int index) {
@@ -31,12 +76,12 @@ public:
 		return *t;
 	}
 
-	void operator*(double n) {
+	void operator*=(double n) {
 		for (unsigned int i = 0; i < _vector.size(); ++i)
 			*(_vector[i]) = *(_vector[i]) * n;
 	}
 
-	void operator*(const DataSeries<T>& other) {
+	void operator*=(const DataSeries<T>& other) {
 		std::vector<std::shared_ptr<T>> tempVector;
 		for (unsigned int i = 0; i < _vector.size(); ++i)
 			tempVector.push_back(std::make_shared<T>(*(_vector[i]) * *(other._vector[i])));
@@ -45,12 +90,12 @@ public:
 		_vector = tempVector;
 	}
 
-	void operator/(double n) {
+	void operator/=(double n) {
 		for (unsigned int i = 0; i < _vector.size(); ++i)
 			*(_vector[i]) = *(_vector[i]) / n;
 	}
 
-	void operator/(const DataSeries<T>& other) {
+	void operator/=(const DataSeries<T>& other) {
 		std::vector<std::shared_ptr<T>> tempVector;
 		for (unsigned int i = 0; i < _vector.size(); ++i)
 			tempVector.push_back(std::make_shared<T>(*(_vector[i]) / *(other._vector[i])));
@@ -65,6 +110,14 @@ public:
 		typename std::vector<std::shared_ptr<T>>::iterator it;
 		
 		T& operator*() {
+			if (*it == nullptr) {
+				std::cout << "NaN";
+			
+				T temp;
+				memset(&temp, 0, sizeof(T));
+				return temp;
+			}
+
 			return **it;
 		}
 
@@ -96,10 +149,21 @@ public:
 		DataSeries<T> shiftedDataSeries;
 		std::vector<std::shared_ptr<T>> tempVector;
 
-		for (unsigned int i = -shift; i < _vector.size(); ++i) {
+		if (shift < 0) {
+			shift *= -1;
+		}
+		else {
+			for (unsigned int i = 0; i < shift; ++i) {
+				tempVector.push_back(std::shared_ptr<T>());
+			}
+			shift = 0;
+		}
+
+		for (unsigned int i = shift; i < _vector.size(); ++i) {
 			tempVector.push_back(std::make_shared<T>(*(_vector[i])));
 		}
 		
+		shiftedDataSeries._vector.clear();
 		shiftedDataSeries._vector = tempVector;
 		return shiftedDataSeries;
 	}
@@ -115,7 +179,7 @@ int main(int argc, char* argv[]) {
 	series["2019/5/8"] = 1.23;
 
 	DataSeries<double> returns = series.shift(-1);
-	returns / series;
+	returns /= series;
 
 	for (const auto& i : returns)
 		std::cout << i << std::endl;
@@ -130,20 +194,25 @@ int main(int argc, char* argv[]) {
 	std::cout << std::endl;
 
 	
-	series / series[0];
-	series * 2;
+	series /= series[0];
+	series *= 2;
 
 	for (const auto& i : series)
 		std::cout << i << std::endl;
 
 	std::cout << std::endl;
 
-	series * series;
+	series *= series;
 
 	for (const auto& i : series)
 		std::cout << i << std::endl;
 
-	DataSeries<double> shifted = series.shift(-1);
+	std::cout <<  std::endl;
+
+	DataSeries<double> shifted = series.shift(1);
+
+	for (const auto& i : shifted)
+		std::cout << i << std::endl;
 
 	std::cin.get();
 
